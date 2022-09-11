cfc700b4e1e50d2142bf7ed281eafcc287c0c091
Keefe Vuong <keefevuong123@gmail.com>
2022-09-11 15:01:21 +1000

    Fixed bug with canMove on rooks

--- pieces/Rook.java
+++ pieces/Rook.java
@@ -11,9 +11,18 @@ public class Rook extends Pieces{
         if (dX == 0 && dY != 0) {
             System.out.println(y);
             System.out.println(dY);
-            for (int i = y; i > y - dY; i--) {
-                if (!board[Math.abs(i)][this.x].toString().equals("✕") && i != y) {
-                    return false;
+            if (y > dY) { 
+                for (int i = y; i > y - dY; i--) {
+                    if (!board[Math.abs(i)][this.x].toString().equals("✕") && i != y) {
+                        return false;
+                    }
+                }
+            }
+            else {
+                for (int i = y; i < y + dY; i++) {
+                    if (!board[Math.abs(i)][this.x].toString().equals("✕") && i != y) {
+                        return false;
+                    }
                 }
             }
             return true;
@@ -21,56 +30,26 @@ public class Rook extends Pieces{
         else if (dY == 0 && dX != 0) {
             System.out.println(x);
             System.out.println(dX);
-            for (int i = x; i > x - dX; i--) {
-                System.out.println(board[y][Math.abs(i)].toString());
-                if (!board[y][Math.abs(i)].toString().equals("✕") && i != x) {
-                    return false;
+            if (x > dX) {
+                for (int i = x; i > x - dX; i--) {
+                    System.out.println(board[y][Math.abs(i)].toString());
+                    if (!board[y][Math.abs(i)].toString().equals("✕") && i != x) {
+                        return false;
+                    }
+                }  
+            }
+            else {
+                for (int i = x; i < x + dX; i++) {
+                    System.out.println(board[y][Math.abs(i)].toString());
+                    if (!board[y][Math.abs(i)].toString().equals("✕") && i != x) {
+                        return false;
+                    }
                 }
             }
             return true;
         }
 
         return false;
-
-        // if (this.x != finalX && this.y != finalY) {
-        //     return false;
-        // }
-
-        // if (this.x == finalX) {
-        //     if (y < finalY) {
-        //         for (int i = y + 1; i < finalY; i++) {
-        //             if (!board[i][this.x].toString().equals("X")) {
-        //                 return false;
-        //             }
-        //         }
-        //     }
-        //     else {
-        //         for (int i = y - 1; i > finalY; i--) {
-        //             if (!board[i][this.x].toString().equals("X")) {
-        //                 return false;
-        //             }
-        //         }
-        //     }
-        // }
-
-        // if (this.y == finalY) {
-        //     if (x < finalX) {
-        //         for (int i = x + 1; i < finalX; i++) {
-        //             if (!board[this.y][i].toString().equals("X")) {
-        //                 return false;
-        //             }
-        //         }
-        //     }
-        //     else {
-        //         for (int i = x - 1; i > finalX; i--) {
-        //             if (!board[this.y][i].toString().equals("X")) {
-        //                 return false;
-        //             }
-        //         }
-        //     }
-        // }
-
-        // return true;
     }
 
     @Override
